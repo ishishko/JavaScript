41760e9b292aa3cd7c664ad322aee1b12fcc8b5a
Ishishko <shishkoignacio@gmail.com>
2022-10-17 11:40:17 -0300

    Clase 04 Completada

--- 02-JS-en-la-WEB/app.js
+++ 02-JS-en-la-WEB/app.js
@@ -1,25 +1,41 @@
-const btn = document.querySelector("[data-form-btn]");
+(() => {
+  const btn = document.querySelector("[data-form-btn]");
 
-const createTask = (e) => {
-  e.preventDefault();
-  const input = document.querySelector("[data-form-input]");
-  const value = input.value;
-  const list = document.querySelector("[data-list]");
-  const task = document.createElement("li");
-  task.classList.add("card");
-  input.value = "";
-  const content = `
-    <div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
+  const createTask = (e) => {
+    e.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
+
+    const taskContent = document.createElement("div");
+    const titleTask = document.createElement("span");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(check());
+    taskContent.appendChild(titleTask);
+    const content = `
     <i class="fas fa-trash-alt trashIcon icon"></i>
   `;
-  task.innerHTML = content;
-  list.appendChild(task);
-  console.log(content);
-};
+    task.appendChild(taskContent);
+    // task.innerHTML = content;
+    list.appendChild(task);
+  };
 
-console.log(btn);
+  btn.addEventListener("click", createTask);
 
-btn.addEventListener("click", createTask);
+  const check = () => {
+    const i = document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
+    return i;
+  };
+
+  const completeTask = (e) => {
+    e.target.classList.toggle("fas");
+    e.target.classList.toggle("far");
+    e.target.classList.toggle("completeIcon");
+  };
+})();
